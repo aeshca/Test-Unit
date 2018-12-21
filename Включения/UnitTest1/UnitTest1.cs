99e49b75d2f77a33b7595e9ebcf15fd95b71e631
aeshca <45589813+aeshca@users.noreply.github.com>
2018-12-21 08:21:12 +0500

    Unit Test Done

---
+++ Включения/UnitTest1/UnitTest1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Включения;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // исходные данные
+            string str = "там за морями за лесами";
+            char i = 'а';
+            int expected = 4;
+            Program g = new Program();
+            int rezult = g.vxod(str,i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // исходные данные
+            string str = "how do you do";
+            char i = 'o';
+            int expected = 4;
+            Program g = new Program();
+            int rezult = g.vxod(str, i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            // исходные данные
+            string str = "4n0934ftn45385897n";
+            char i = 'g';
+            int expected = 0;
+            Program g = new Program();
+            int rezult = g.vxod(str, i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            // исходные данные
+            string str = "";
+            char i = '5';
+            int expected = 0;
+            Program g = new Program();
+            int rezult = g.vxod(str, i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            // исходные данные
+            string str = "!@#$#$#$@#$$%^#$@#$!#@%*/*+/*/";
+            char i = '@';
+            int expected = 4;
+            Program g = new Program();
+            int rezult = g.vxod(str, i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            // исходные данные
+            string str = "45304264620787766767844574878777979";
+            char i = '9';
+            int expected = 2;
+            Program g = new Program();
+            int rezult = g.vxod(str, i);
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, rezult);
+        }
+    }
+}
---
+++ Включения/UnitTest1/Включения/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Включения
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            while(true)
+            {
+                Console.WriteLine("Введите строку:");
+                string str = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Введите символ:");
+                char ch = Convert.ToChar(Console.ReadLine());
+                int coutn = p.vxod(str, ch);
+                Console.WriteLine("Количество: " + coutn);
+                Console.ReadKey();
+            }
+            
+        }
+
+        public int vxod(string str, char ch)
+        {
+             int coutn = str.Where(x => x == ch).Count();
+             return coutn;
+        }
+    }
+}
